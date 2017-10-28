95a7c530a6e015554dedb746af3773d4bd8d755c
dreamless64 <dreamless64@gmail.com>
2017-10-28 17:06:00 +0900

    으음~

--- mobile/js/common.js
+++ mobile/js/common.js
@@ -1,22 +1,25 @@
 (function($){
 $(document).ready(function(){
 // !! code start !!
-var $navitem = $('.main_nav_ul .main_nav_item');
-var $navbar = $('.main_nav > .fixnav_bar');
 
-$navitem.on('click',function(){
+// 상단 네비게이션
+var $navitem = $('.main_nav_ul .main_nav_item');
+var $navbar = $('.fixnav_bar');
+
+var navClick = function(){
+  var itemW =  (100 / $(window).width()) * $navitem.width();
+  var index = $(this).index();
+  $navbar.css('width', itemW + '%');
   $('.main_nav_ul .main_nav_item').removeClass('select');
   $(this).addClass('select');
-  // console.log($(this).index());
-  // var thisW = $(this).find('a').width();
-  // console.log(thisW);
-  // var minusW = ($('.main_nav_item').width()) - thisW;
+  if(index < 0){index = 0;}
   $navbar.animate({
-    // 'width': thisW,
-    'left': ($(this).index() * 12.5) + '%'
+    'left': index * itemW + '%'
   },300,'easeInOutCubic');
-});
+}
+$navitem.on('click', navClick);
 
+// 하단 네비게이션
 var $under = $('.underbox');
 var $underItem = $('.underbox .underbar_list li div');
 
@@ -26,11 +29,11 @@ $underItem.on('click', function(){
   $(this).addClass('select');
 })
 
-
-
 $('a').on('click',function(evt){
   evt.preventDefault();
 })
+
 // document
+navClick();
 });//ready
 })(jQuery);
