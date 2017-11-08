e3e3e5fb9baf1e5d899dc76569d6e183c655ac2c
dreamless64 <dreamless64@gmail.com>
2017-11-08 22:23:39 +0900

    ㅇㅡ어어

--- web_intro/js/script.js
+++ web_intro/js/script.js
@@ -2,18 +2,27 @@
 $(document).ready(function(){
 // !! code start !!
 
+
+// 네비게이션
+$('.nav_item > *').on('click',function(){
+  console.log($(this).parent('.nav_item').index());
+});
+
+
 // slider크기 자동조절
 var sliderSize = function(){
   var $item = $('.slider_item');
   var $ul = $('.slider_ul');
   var winW = $(window).width();
   var itemW = winW / 3;
-  var ulW = itemW * ($item.length);
+  // var ulW = itemW * ($item.length-1);
   $item.width(itemW).height(itemW);
-  $ul.width(ulW);
+  // $ul.width(ulW);
 };
 // document
+
 $(window).resize(sliderSize);
 sliderSize();
+
 });//ready
 })(jQuery);
