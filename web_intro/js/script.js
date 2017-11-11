67e5492e4de72f6e02750dcf149a48c644206297
dreamless64 <dreamless64@gmail.com>
2017-11-11 18:22:54 +0900

    what 완성

--- web_intro/js/script.js
+++ web_intro/js/script.js
@@ -9,8 +9,6 @@ $('.nav_item > *').on('click',function(){
 });
 
 
-
-
 // section slider크기 자동조절
 var sliderSize = function(){
   var $item = $('.slider_item'),
@@ -24,29 +22,50 @@ var sliderSize = function(){
 };
 
 
-
-
 // what, what_bracket 등등
 var bracketAnimate = function(){
   var $box = $('.what_bracketbox'),
       $bracket = $('.what_bracket'),
       $cont = $('.what_content'),
-      $contUl = $('.what_content_ul');
-  var timer;
-  var _cuId = 0;
+      $contUl = $('.what_content_ul'),
+      $contItem = $('.what_item'),
+      $dotItem = $('.what_dot_item'),
+      $pItem = $('.what_p_item');
+
+  var timer,
+      delayTimer,
+      _cuId = 0;
+
   var contentInterval = function(){
-    timer = setInterval(function(){
-      ++_cuId;
-      if(_cuId > $contUl.length){
-        _cuId = 0 ;
-      }
-      console.log(_cuId);
-    },500);
-  }
+    var whatAnimate = function(){
+      $contUl.stop().animate({
+        'top':'-'+ _cuId * $('.what_item').height()
+      },500,'easeInOutCubic');
+      $dotItem.removeClass('active');
+      $dotItem.eq(_cuId).addClass('active');
+      $pItem.removeClass('active');
+      $pItem.eq(_cuId).addClass('active');
+    };
+    $dotItem.on('click',function(){
+      _cuId = $(this).index();
+      whatAnimate();
+      clearInterval(timer);
+      clearTimeout(delayTimer);
+      delayTimer = setTimeout(whatTimerset,4000);
+    });
+    var whatTimerset =  function(){
+      timer = setInterval(function(){
+        ++_cuId;
+        if(_cuId >= $contItem.length){_cuId = 0;}
+        whatAnimate();
+      },3000);
+    }
+    whatTimerset();
+  };
+
   $cont.animate({
     'width':'600px'
   },1300,'easeOutBack',contentInterval);
-  // console.log('sad');
 }
 
 
